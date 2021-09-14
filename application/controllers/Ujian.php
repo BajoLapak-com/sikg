78c9e5a57d87f8e1f0ba853786b97f366c96677f
Yohanes Erros Sahu <duragonprojek@gmail.com>
2021-09-14 12:23:47 +0800

    Commit

--- application/controllers/Ujian.php
+++ application/controllers/Ujian.php
@@ -1,1 +1,19 @@
 <!-- Goldian -->
+<?php 
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Ujian extends CI_Controller {
+
+    public function index($nama=null)
+    {
+        echo $nama ; 
+        die(); 
+        
+    }
+
+}
+
+/* End of file Ujian.php */
+
+?>
